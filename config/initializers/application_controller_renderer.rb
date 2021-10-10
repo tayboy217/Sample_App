4355103a924ca20b925ebdc6dfb254e92c3c27f8
EC2 Default User <ec2-user@ip-172-31-32-65.ec2.internal>
2021-10-10 05:43:16 +0000

    [Add All]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7f24e77fdd46fa7edb5e5323f48ebb70f87c7025114568f394fb79693615aa0d0c8aa6a7ca74650bc76453da656d54eb66babc3b4a2107214722713fcb7adf82'
--- db/migrate/20211008075617_create_lists.rb
+++ db/migrate/20211008075617_create_lists.rb
@@ -2,9 +2,9 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
 # 以下の2行に追加
-　　  t.string :title
+      t.string :title
       t.string :body
-      
+
       t.timestamps
     end
   end
